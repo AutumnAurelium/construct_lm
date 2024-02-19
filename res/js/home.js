a4a0902228b947a051a9427a4026aa3b9a0e7727
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2024-02-19 12:44:43 -0500

    initial commit

---
+++ res/js/home.js
@@ -0,0 +1,129 @@
+let lockInput = false;
+let shiftHeld = false;
+let lockSystem = false;
+let messages = [
+
+]
+
+function escapeHtml(unsafe) {
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;")
+        .replace(/\n/g, "<br />");
+}
+
+function addMessage(msg) {
+    let role = msg.role;
+    let text = msg["content"];
+    let icon = "";
+    if(role === "user") {
+        icon = "fa-user";
+    } else if(role === "assistant") {
+        icon = "fa-robot";
+    } else if(role === "tool") {
+        icon = "fa-computer";
+    }
+
+    let color = "light";
+    if (role === "user") {
+        color = "bg";
+    }
+
+    console.log(text);
+
+    if(typeof text === "string") {
+        $("#messages").append("<div class=\"row bg-" + color + "\">\n" +
+            "            <div class=\"text-center col-xs\"><span class=\"fa-solid " + icon + " fa-fw\"></span></div>\n" +
+            "            <div class=\"col-xl\">" + escapeHtml(text) + "</div>\n" +
+            "        </div>");
+    }
+}
+
+async function getCompletion(messages) {
+    const response = await fetch("/api/chat/completion", {
+        method: "POST",
+        cache: "no-cache",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            tools: [],
+            messages: messages
+        })
+    });
+
+    return await response.json();
+}
+
+$(document).ready(() => {
+    $("#systemInput").val(localStorage.getItem("system"));
+
+    $("#messageInput").on("keydown", event => {
+        if (event.which === 16) {
+            shiftHeld = true;
+        }
+
+        textarea = $("#messageInput");
+
+        if (event.which === 13) {
+            if (!shiftHeld) {
+                if(messages.length === 0) {
+                    sysInput = $("#systemInput");
+                    messages.push(
+                        {"role": "system", "content": sysInput.val()}
+                    );
+                    lockSystem = true;
+                    sysInput.prop("disabled", true);
+                }
+
+                if(textarea.val() !== "") {
+                    msg = {"role": "user", "content": textarea.val()};
+                    messages.push(msg)
+                    addMessage(msg);
+                }
+                textarea.val("");
+                event.preventDefault();
+
+                lockInput = true;
+                textarea.prop("disabled", true);
+                textarea.prop("placeholder", "Awaiting response...");
+
+                // fetch from local API
+                getCompletion(messages).then(response => {
+                    response.forEach((msg) => {
+                        messages.push(msg)
+                        console.log(msg);
+                        addMessage(msg);
+                    });
+                    lockInput = false;
+                    textarea.prop("disabled", false);
+                    textarea.focus();
+                    textarea.prop("placeholder", "");
+                }).catch(error => {
+                    textarea.prop("placeholder", "API error. Please retry. (" + error + ")");
+                    throw error;
+                });
+            }
+        }
+    }).on("keyup", event => {
+        if (event.which === 16) {
+            shiftHeld = false;
+        }
+    });
+
+    $("#reset").on("click", event => {
+        messages = [];
+        lockSystem = false;
+        $("#systemInput").prop("disabled", false);
+        $("#messages").empty();
+    });
+
+    $("#systemInput").on("keyup", event => {
+        localStorage.setItem("system", $("#systemInput").val());
+    });
+
+    $("textarea").autosize();
+});
